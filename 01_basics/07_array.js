c6de6c53ff72bd8c659121d10de0dbf4a73be199
intzar786 <80245372+intzar786@users.noreply.github.com>
2023-09-15 03:31:21 +0000

    array changes

--- 01_basics/07_array.js
+++ 01_basics/07_array.js
@@ -1,2 +1,15 @@
 let myArr = [1,3,5,2,7]
 console.log(myArr);
+
+myArr.push(4)
+console.log(myArr); 
+
+myArr.pop()
+console.log(myArr)
+
+myArr.unshift(5)
+console.log(myArr);
+
+myArr.shift()
+console.log(myArr);
+
