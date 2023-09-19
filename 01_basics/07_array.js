18eb81fdd07dbb33a3f656f7c3be7c9b91a7c386
intzar786 <80245372+intzar786@users.noreply.github.com>
2023-09-19 12:15:27 +0000

    test

--- 01_basics/07_array.js
+++ 01_basics/07_array.js
@@ -1,15 +1,22 @@
+let arr = Array(20);
+//console.log(arr.length); // 20
+
 let myArr = [1,3,5,2,7]
-console.log(myArr);
+//console.log(myArr);
 
 myArr.push(4)
-console.log(myArr); 
+//console.log(myArr); 
 
 myArr.pop()
-console.log(myArr)
+//console.log(myArr)
 
 myArr.unshift(5)
-console.log(myArr);
+//console.log(myArr);
 
 myArr.shift()
-console.log(myArr);
+//console.log(myArr);
+const concetArray1 = ['a','b']
+const concetArray2 = [false,['x','y','z']]
+const finalArr = myArr.concat(concetArray1, concetArray2) 
 
+console.log(finalArr);
